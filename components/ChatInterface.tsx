385f1ee9c9a8f784eb15f21034c5a9bbaebab716
burack <omerfaruk2695@gmail.com>
2025-05-01 18:09:34 +0200

    update

--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -199,6 +199,7 @@ const ChatInterface: React.FC = () => {
                   className="flex-1 px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white disabled:opacity-50"
                 />
                 <button
+                  type='button'
                   onClick={(e) => clearMessages(e)}
                   disabled={isSending || messages.length === 0}
                   className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
